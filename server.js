ac7cc94a58f25f16357b0e2a8d1f64c06bde2f75
nagyb3 <nagybence2003@gmail.com>
2023-07-31 21:49:37 +0200

    use cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const port = process.env.PORT || 5000;
 const asyncHandler = require('express-async-handler');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -10,6 +11,7 @@ const User = require('./models/user');
 const Message = require('./models/message');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
